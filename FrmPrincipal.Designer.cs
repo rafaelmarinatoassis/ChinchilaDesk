e2d64b229a80ba53ac5e3ccb6fe4a803233a6c2e
Senai <Senai@CEPEAS21104194>
2025-05-09 21:22:17 -0300

    Tela inicial de Login criada

--- FrmPrincipal.Designer.cs
+++ FrmPrincipal.Designer.cs
@@ -29,26 +29,95 @@
         private void InitializeComponent()
         {
             this.btnEntrar = new System.Windows.Forms.Button();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panelLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(257, 141);
+            this.btnEntrar.Location = new System.Drawing.Point(30, 298);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(334, 130);
+            this.btnEntrar.Size = new System.Drawing.Size(237, 42);
             this.btnEntrar.TabIndex = 0;
-            this.btnEntrar.Text = "Entrar Aqui";
+            this.btnEntrar.Text = "Entrar";
             this.btnEntrar.UseVisualStyleBackColor = true;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsuario.Location = new System.Drawing.Point(25, 150);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(93, 25);
+            this.labelUsuario.TabIndex = 1;
+            this.labelUsuario.Text = "Usuário";
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLogin.Controls.Add(this.textBox2);
+            this.panelLogin.Controls.Add(this.textBox1);
+            this.panelLogin.Controls.Add(this.labelSenha);
+            this.panelLogin.Controls.Add(this.label1);
+            this.panelLogin.Controls.Add(this.btnEntrar);
+            this.panelLogin.Controls.Add(this.labelUsuario);
+            this.panelLogin.Location = new System.Drawing.Point(12, 12);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(292, 628);
+            this.panelLogin.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chinchila Desk";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.Location = new System.Drawing.Point(25, 219);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(79, 25);
+            this.labelSenha.TabIndex = 3;
+            this.labelSenha.Text = "Senha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(30, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 29);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(30, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 29);
+            this.textBox2.TabIndex = 5;
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnEntrar);
+            this.ClientSize = new System.Drawing.Size(1235, 652);
+            this.Controls.Add(this.panelLogin);
             this.Name = "FrmPrincipal";
-            this.Text = "Formulario Principal";
+            this.Text = "Chinchila Desk";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +125,12 @@
         #endregion
 
         private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.Label label1;
     }
 }
 
